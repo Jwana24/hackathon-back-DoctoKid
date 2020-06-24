ffc889634a8cf063a8f4d5f271733fbc5e23dc19
PierreVelluet <velluetp@gmail.com>
2020-06-24 14:34:46 +0200

    job done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8080;  
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const connection = require('./config.js')
 
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const config = require('./config.json')
-const mysql = require('mysql')
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(morgan('dev'))
+
+app.listen(port, (err) => {
+    if (err) {
+      throw new Error('Something bad happened...');
+    }
+    console.log(`Server is listening on ${port}`);
+  });
