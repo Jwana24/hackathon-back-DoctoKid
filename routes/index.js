eb280d30a76736da3eb68eb6f8d15436e1a00f13
Jwana24 <detrieux.btscom13@gmail.com>
2020-06-24 22:47:39 +0200

    add dinosaurs route

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 // import route files
-const users = require('./users')
-const specialists = require('./specialists')
-const deseases = require('./deseases')
+const users = require('./users');
+const specialists = require('./specialists');
+const deseases = require('./deseases');
+const dinosaurs = require('./dinosaurs');
 
 // init routes
 
 router.use('/users', users);
 router.use('/specialists', specialists);
 router.use('/deseases', deseases);
+router.use('/dinosaurs', dinosaurs);
 
 module.exports = router;
